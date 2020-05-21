a16ad2228ce9e2d25a1a3c5cbc8c3912b202db82
nailesh jadhav <nailesh.jadhav@gmail.com>
2020-05-21 14:22:24 +0530

    Updated column values

--- src/main/resources/schema-mysql.sql
+++ src/main/resources/schema-mysql.sql
@@ -2,23 +2,22 @@ create schema if not exists samurai;
 use samurai;
 create schema if not exists samurai;
 use samurai;
-CREATE TABLE IF NOT EXISTS `samurai_rpa` (
-    samurai_rpa_id BIGINT(10) NOT NULL AUTO_INCREMENT,
-    zeva_request_id VARCHAR(20),
-    user_name VARCHAR(20),
-    user_email VARCHAR(50),
-    request_date_time DATE,
-    severity VARCHAR(10),
-    impact VARCHAR(10),
-    eform_id BIGINT(10),
-    type_of_request VARCHAR(10),
-    resolution_response  varchar(1000),
-    resolution_platform varchar(50),
-    solution_type varchar(50),
-    eform_status_by_platform VARCHAR(20),
-    eform_status_update_date DATE,
-    platform_remarks VARCHAR(20),
-    db_connection_url VARCHAR(50),
-    PRIMARY KEY(samurai_rpa_id)
+CREATE TABLE `samurai_rpa` (
+  `samurai_rpa_id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `zeva_request_id` varchar(20) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `user_email` varchar(100) DEFAULT NULL,
+  `request_date_time` date DEFAULT NULL,
+  `severity` varchar(50) DEFAULT NULL,
+  `impact` varchar(50) DEFAULT NULL,
+  `eform_id` bigint(20) DEFAULT NULL,
+  `type_of_request` varchar(50) DEFAULT NULL,
+  `resolution_response` varchar(5000) DEFAULT NULL,
+  `resolution_platform` varchar(50) DEFAULT NULL,
+  `solution_type` varchar(50) DEFAULT NULL,
+  `eform_status_by_platform` varchar(20) DEFAULT NULL,
+  `eform_status_update_date` date DEFAULT NULL,
+  `platform_remarks` varchar(20) DEFAULT NULL,
+  `db_connection_url` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`samurai_rpa_id`)
 );
-
